905738245281e94c7ab18e55f8f5991499d96416
DESKTOP-3HTT3UP\Walter <xx159357222@gmail.com>
2020-09-08 22:19:45 +0800

    update routerfix ie11update router

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,8 +12,8 @@ export default {
    ** Nuxt target
    ** See https://nuxtjs.org/api/configuration-target
    */
-  target: 'server',
-  // target: 'static',
+  // target: 'server',
+  target: 'static',
   /*
    ** Headers of the page
    ** See https://nuxtjs.org/api/configuration-head
